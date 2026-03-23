33cd1d3195c18ae0926b1d76de4cfcf57004cd4a
xstarriver <ers13542785lol@gmail.com>
2026-03-23 14:47:09 +0800

    updates

--- src/components/layout/LayoutShell.tsx
+++ src/components/layout/LayoutShell.tsx
@@ -13,11 +13,11 @@ export function LayoutShell({ children }: { children: React.ReactNode }) {
     }
 
     return (
-        <div className="flex w-full min-h-screen overflow-hidden">
+        <div className="flex w-full min-h-screen">
             <Sidebar />
             <div className="flex-1 flex flex-col min-w-0">
                 <Header />
-                <main className="p-4 sm:p-6 md:p-8 flex-1 overflow-x-hidden min-w-0">
+                <main className="p-4 sm:p-6 md:p-8 flex-1 min-w-0">
                     {children}
                 </main>
             </div>
